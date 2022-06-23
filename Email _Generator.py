3913286eb3b777e470a8ad42d0f337d62411f517
Mohammed  Hassan <64578019+mgkw@users.noreply.github.com>
2022-06-23 21:10:59 +0300

    Add files via upload
    Email Generator

---
+++ Email _Generator.py
@@ -0,0 +1,48 @@
+# Python version: 3.9.7
+# Date: 6/23/2022
+# Author: Mohammed Hassan
+# Description: Email Generator
+# Telegram channel: https://t.me/python_2
+
+#Libraries
+import string
+import random
+
+#Msg start
+print("Telegram Channel:"'\n'"https://t.me/python_2")
+print("1 - yahoo"'\n'"2 - gmail"'\n'"3- Hotmail"'\n'"Choose any domail")
+
+#Variables
+letters = string.ascii_lowercase
+email = int(input("domain : "))
+Generator = int(input("How many emails: "))
+STOP = 0
+Letters = 0
+File_Email = open("email.txt", "w")
+ranges = int(input("email characters range : "))
+
+#Loop
+while True:
+
+
+    if email == 1:
+        File_Email.write(''.join(random.choice(letters) for i in range(ranges))+'@yahoo.com'+'\n')
+        STOP += 1
+        if STOP == Generator:
+            break
+
+    if email == 2:
+        File_Email.write(''.join(random.choice(letters) for i in range(ranges)) + '@gmail.com' + '\n')
+        STOP += 1
+        if STOP == Generator:
+            break
+
+    if email == 3:
+        File_Email.write(''.join(random.choice(letters) for i in range(ranges)) + '@Hotmail.com' + '\n')
+        STOP += 1
+        if STOP == Generator:
+            break
+
+#Msg end
+print("Done Generator!")
+
